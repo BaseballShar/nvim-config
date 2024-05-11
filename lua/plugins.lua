f632765c555c2963367a9ee6c948c85a4e59e54c
Baseball Shar <workingcth@gmail.com>
2024-05-11 18:58:23 +0800

    Done lsp-cmp next step is organise configs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,6 +49,20 @@ require("lazy").setup({
       keymap('n', '<Leader>gk', builtin.keymaps)
     end,
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown{}
+          }
+        }
+      })
+      telescope.load_extension("ui-select")
+    end,
+  },
   "junegunn/fzf",
   "junegunn/fzf.vim",
   "nvim-tree/nvim-tree.lua",
@@ -77,7 +91,7 @@ require("lazy").setup({
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
+        ensure_installed = {"lua_ls", "tsserver"}
       })
     end,
   },
@@ -87,6 +101,7 @@ require("lazy").setup({
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
 
       vim.keymap.set('n', '<Leader>ml', ':LspInfo<CR>')
       vim.keymap.set('n', 'K', vim.lsp.buf.hover)
@@ -97,6 +112,78 @@ require("lazy").setup({
       vim.keymap.set('n', '<Space>ca', vim.lsp.buf.code_action)
     end,
   },
+
+  -- Snippet plugins
+  "neovim/nvim-lspconfig",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require'cmp'
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For luasnip users.
+        }, {
+            { name = 'buffer' },
+          })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+          }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+      -- Set up lspconfig.
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      require('lspconfig')['lua_ls'].setup {
+        capabilities = capabilities
+      }
+      require('lspconfig')['tsserver'].setup {
+        capabilities = capabilities
+      }
+    end,
+  },
+
+  -- LuaSnip engine
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
 
   -- Utility plugins
   { "akinsho/toggleterm.nvim", tag = "v2.11.0" },
