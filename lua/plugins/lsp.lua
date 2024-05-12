099d4eb0061ba2270e651c2ec5159cabdfa29c31
Baseball Shar <workingcth@gmail.com>
2024-05-12 16:51:50 +0800

    Loaded some pre-defined snippets to Luasnip

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -149,6 +149,16 @@ return {
   },
 
   -- LuaSnip engine
-  "L3MON4D3/LuaSnip",
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      local ls = require("luasnip")
+      vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump( 1) end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<C-k>", function() ls.jump(-1) end, {silent = true})
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
   "saadparwaiz1/cmp_luasnip",
 }
