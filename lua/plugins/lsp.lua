8da96b0283d21f14359a2c3ce11813dd6ad44bb0
Baseball Shar <workingcth@gmail.com>
2024-05-12 13:15:52 +0800

    Lazy load a portion of nvim-cmp upon InsertEnter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,13 +75,26 @@ return {
   },
 
   -- Snippet plugins
-  "neovim/nvim-lspconfig",
   "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
+  {
+    "hrsh7th/cmp-buffer",
+    event = "InsertEnter"
+  },
+  {
+    "hrsh7th/cmp-path",
+    event = "InsertEnter"
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    event = "InsertEnter"
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = "InsertEnter"
+  },
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     config = function()
       local cmp = require'cmp'
 
