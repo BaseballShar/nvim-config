2be0942f825061ce11c151a751185d9a7f0c59c0
Baseball Shar <workingcth@gmail.com>
2024-05-12 13:48:19 +0800

    Used lualine to replace bufferline

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -1,23 +1,20 @@
 return {
-  -- A decent buffer indicator for vim
-  {
-    "akinsho/bufferline.nvim",
-    version = "4.5.x",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup()
-    end
-  },
-
   -- A Nice and highly customisible statusline for vim
   {
     "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup({
         sections = {
-          lualine_b = {"branch", "diff"},
-          lualine_c = {"g:coc_status"},
-          lualine_x = {"diagnostics"}
+          lualine_b = {"filename"},
+          lualine_c = {},
+          lualine_x = {"diagnostics"},
+          lualine_y = {"diff", "branch"},
+          lualine_z = {"location"},
+          -- lualine_z = {"diagnostics"}
+        },
+        options = {
+          component_separators = "",
+          -- section_separators = "",
         }
       })
     end,
