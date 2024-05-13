f2b1db41ee178b7788cf9977753879d0f559804d
Baseball Shar <workingcth@gmail.com>
2024-05-13 23:41:40 +0800

    Better lualine and completion window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,8 +104,8 @@ return {
           end,
         },
         window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item(),
