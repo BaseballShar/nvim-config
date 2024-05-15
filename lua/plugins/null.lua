ebf0f0f901e8b3e3a4716ef514c5da05edce82d5
Baseball Shar <workingcth@gmail.com>
2024-05-15 11:11:13 +0800

    Added javascript formatting

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -3,22 +3,31 @@ local keymap = vim.keymap.set
 local opts = { buffer = true, silent = true }
 
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+			},
+		})
 
-    -- Autocmd formatting
-    autocmd("Filetype", {
-      pattern = "python",
-      callback = function()
-        keymap("n", "<Leader>cf", ":%!autopep8 -<CR>", opts)
-        keymap("x", "<Leader>cf", ":'<,'>!autopep8 -<CR>", opts)
-      end,
-    })
-  end,
+		-- Autocmd formatting
+		autocmd("Filetype", {
+			pattern = "python",
+			callback = function()
+				keymap("n", "<Leader>cf", ":%!autopep8 -<CR>", opts)
+				keymap("x", "<Leader>cf", ":'<,'>!autopep8 -<CR>", opts)
+			end,
+		})
+
+		-- Javascript formatting
+		autocmd("Filetype", {
+			pattern = { "javascript", "typescript" },
+			callback = function()
+				keymap("n", "<Leader>cf", ":%!prettier --stdin-filepath %<CR>", opts)
+				keymap("x", "<Leader>cf", ":'<,'>!prettier --stdin-filepath %<CR>", opts)
+			end,
+		})
+	end,
 }
