2fe7251995ce3d39cd0d5cde48ec148a2edd5245
Baseball Shar <workingcth@gmail.com>
2024-05-15 10:49:08 +0800

    Added python formatting via autopep8 autocmd

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -1,3 +1,7 @@
+local autocmd = vim.api.nvim_create_autocmd
+local keymap = vim.keymap.set
+local opts = { buffer = true, silent = true }
+
 return {
   "nvimtools/none-ls.nvim",
   config = function()
@@ -5,10 +9,16 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.formatting.ormolu,
-        -- null_ls.builtins.formatting.autoflake,
       },
+    })
+
+    -- Autocmd formatting
+    autocmd("Filetype", {
+      pattern = "python",
+      callback = function()
+        keymap("n", "<Leader>cf", ":%!autopep8 -<CR>", opts)
+        keymap("x", "<Leader>cf", ":'<,'>!autopep8 -<CR>", opts)
+      end,
     })
   end,
 }
