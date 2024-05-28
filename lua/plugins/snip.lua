2e818f2b2d02c67121291cae49056e46f1681da7
Baseball Shar <workingcth@gmail.com>
2024-05-28 12:53:19 +0800

    Added react snippets

--- lua/plugins/snip.lua
+++ lua/plugins/snip.lua
@@ -2,7 +2,10 @@ return {
 	-- LuaSnip engine
 	{
 		"L3MON4D3/LuaSnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"r5n-dev/vscode-react-javascript-snippets",
+		},
 		config = function()
 			local ls = require("luasnip")
 			vim.keymap.set({ "i", "s" }, "<C-j>", function()
@@ -12,5 +15,10 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
 		end,
+	},
+	-- React snippets
+	{
+		"r5n-dev/vscode-react-javascript-snippets",
+		build = "yarn install --frozen-lockfile && yarn compile",
 	},
 }
