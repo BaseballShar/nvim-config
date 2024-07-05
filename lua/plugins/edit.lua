e3f509ddd2c286f9d80583dd2c0aa9945f62999b
Baseball Shar <workingcth@gmail.com>
2024-07-05 18:17:19 +0800

    Added function textobject

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -19,6 +19,25 @@ return {
     end,
   },
 
+  -- Treesitter textobjects
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+            },
+          },
+        },
+      })
+    end,
+  },
+
   -- Auto close pairs
   {
     "windwp/nvim-autopairs",
